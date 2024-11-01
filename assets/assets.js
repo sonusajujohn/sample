e7bf316eb3245628e398fe5a83e894449d5d08f5
Sonu Saju John <138517688+sonusajujohn@users.noreply.github.com>
2024-11-01 09:00:55 +0530

    Add files via upload

---
+++ assets/assets.js
@@ -0,0 +1,11 @@
+import favicon from './favicon.png'
+import logo from './logo.png'
+import search from './searchicon.png'
+import user from './user.png'
+
+export const assets={
+    favicon,
+    logo,
+    search,
+    user
+}
